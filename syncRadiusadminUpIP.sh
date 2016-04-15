41e2b8b1a94ea351c4c85ddfff9bc1b2c4031fc2
Andy <andrewzirkel@gmail.com>
2016-04-15 14:59:49 -0400

    Update syncRadiusadminUpIP.sh

--- syncRadiusadminUpIP.sh
+++ syncRadiusadminUpIP.sh
@@ -4,4 +4,4 @@ ip="192.168.211.135"
 
 #ubuntu
 rsync -ruv --exclude 'Docs' --exclude 'test' --exclude '.*' --exclude 'nbproject' --exclude '*~' radiusadmin root@$ip:/var/www/
-#rsync -ruv --exclude '.*' radiusadmin root@dori.academic.exeter.k12.pa.us:/var/www/localhost/htdocs
+
